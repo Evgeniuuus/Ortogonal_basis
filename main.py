f89d549cfca7fdb878bcfe7ae8f5688fc125c45c
Evgeniuuus <ezhelinskij@bk.ru>
2025-01-14 14:33:54 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sympy as sp  # Для работы с символьными вычислениями
-import numpy as np  # Для генерации списка X для функций
-import matplotlib.pyplot as plt  # Для отображения графиков
-from math import factorial, e, pi  # Факториал, числа e и pi
-from sympy.abc import t, theta  # Для использования в символьных вычислениях (буквы t и theta)
-from sympy import oo  # Для вычисления несобственных интегралов
+import sympy as sp                           # Для работы с символьными вычислениями
+import numpy as np                           # Для генерации списка X для функций
+import matplotlib.pyplot as plt              # Для отображения графиков
+from math import factorial, e, pi            # Факториал, числа e и pi
+from sympy.abc import t, theta               # Для использования в символьных вычислениях (буквы t и theta)
+from sympy import oo                         # Для вычисления несобственных интегралов
 
 
 # Функции для полиномов
@@ -29,8 +29,8 @@ def chebyshev_poly(n: int):
     # Вычислим все множители
     tn_first_mult = (-2) ** n * factorial(n) / factorial(2 * n)
     tn_second_mult = sp.sympify("sqrt(1-t**2)")
-    tn_third_mult = sp.diff(f"sqrt(1-t**2)**(2*{n}-1)", t, n)  # Производная порядка n по t
-    return sp.simplify(tn_first_mult * tn_second_mult * tn_third_mult)  # Упростить (simplify) возвращаемый результат
+    tn_third_mult = sp.diff(f"sqrt(1-t**2)**(2*{n}-1)", t, n)   # Производная порядка n по t
+    return sp.simplify(tn_first_mult * tn_second_mult * tn_third_mult)     # Упростить (simplify) возвращаемый результат
 
 
 def lagger_poly(n: int):
@@ -41,6 +41,7 @@ def lagger_poly(n: int):
 
     # Данный полином можно представить в виде произведения e^t/n!, и производной порядка n от t^n * e^-t
     # Вычислим все множители
+
     ln_first_mult = sp.sympify(f"{e}**(t)/{factorial(n)}")
     ln_second_mult = sp.diff(sp.sympify(f"t**({n})*{e}**(-t)"), t, n)
     return ln_first_mult * ln_second_mult
@@ -54,27 +55,27 @@ def legandr(xi, func: sp.core.mul.Mul):
     виде)
      Выходные параметры: список Y"""
 
-    n = 4  # Количество необходимых ненулевых членов ряда
-    ft = 0  # Ряд
-    i = 0  # Итератор
-    eps = pow(10, -7)  # Используется для сравнения с нулём
+    n = 4                               # Количество необходимых ненулевых членов ряда
+    ft = 0                              # Ряд
+    i = 0                               # Итератор
+    eps = pow(10, -7)                   # Используется для сравнения с нулём
     while i < n:
-        legandr_polynome = legandr_poly(i)  # Нахождение полинома Лежандра
+        legandr_polynome = legandr_poly(i)                  # Нахождение полинома Лежандра
         cn = ((2 * i + 1) / 2) * sp.N(sp.integrate(func * legandr_polynome, (t, -1, 1)))  # Вычисление Cn
-        if abs(cn) < eps:  # Проверка на ноль
-            n += 1  # Если ноль, то необходимо вычислить ещё один член ряда
-        yp = sp.sympify(cn * legandr_polynome)  # Приведение выражения в понятный для sympy вид
+        if abs(cn) < eps:                                   # Проверка на ноль
+            n += 1                                          # Если ноль, то необходимо вычислить ещё один член ряда
+        yp = sp.sympify(cn * legandr_polynome)              # Приведение выражения в понятный для sympy вид
         yp_lambda = sp.lambdify(t, yp, "numpy")     # Конвертирование в лямбда-функцию
                                                             # (что позволяет очень быстро подставлять значения)
 
-        y = yp_lambda(xi)   # Подстановка Xi
+        y = yp_lambda(xi)                                   # Подстановка Xi
         # Если член ряда не содержит переменных, то значение будет одно (скаляр).
         # Тогда необходимо продлить его на весь промежуток.
         if np.isscalar(y):
             y = np.full_like(xi, y)
-        plt.plot(xi, y, label=f"{yp}")              # Добавление элемента разложения на график
-        ft += yp         # Прибавление элемента к ряду
-        i += 1              # Увеличение итератора
+        plt.plot(xi, y, label=f"{yp}")                # Добавление элемента разложения на график
+        ft += yp                                            # Прибавление элемента к ряду
+        i += 1                                              # Увеличение итератора
     ft_lambda = sp.lambdify(t, ft, "numpy")
     y_ft = ft_lambda(xi)
     return y_ft
@@ -91,9 +92,9 @@ def chebyshev(xi, func: sp.core.mul.Mul):
     # Посчитаем элемент c0
     c0 = 1 / pi
     c0 *= sp.N(sp.integrate(func / sp.sympify("sqrt(1-t**2)"), (t, -1, 1)))
-    n = 4   # Количество необходимых ненулевых членов ряда
-    ft = c0     # Ряд
-    i = 1   # Итератор
+    n = 4                           # Количество необходимых ненулевых членов ряда
+    ft = c0                         # Ряд
+    i = 1                           # Итератор
     eps = pow(10, -7)
 
     # Аналогично как в функции с полиномами Лежандра
@@ -105,7 +106,7 @@ def chebyshev(xi, func: sp.core.mul.Mul):
     # Так как функция в варианте является полиномом третьей степени, она раскладывается только на 3 ненулевых члена.
 
     while i < n:
-        chebyshev_polynome = chebyshev_poly(i) # Нахождение полинома Чебышева
+        chebyshev_polynome = chebyshev_poly(i)          # Нахождение полинома Чебышева
 
         cn = 2 / pi * sp.N(sp.integrate(func * chebyshev_polynome / sp.sympify("sqrt(1-t**2)"), (t, -1, 1)))  # Нахождение Cn
         if abs(cn) < eps:
@@ -115,7 +116,7 @@ def chebyshev(xi, func: sp.core.mul.Mul):
         yp = sp.sympify(cn * chebyshev_polynome)
         yp_lambda = sp.lambdify(t, yp, "numpy")
         y = yp_lambda(xi)
-        plt.plot(xi, y, label=f"{yp}") # Добавление элемента ряда на график
+        plt.plot(xi, y, label=f"{yp}")          # Добавление элемента ряда на график
         ft += yp
         i += 1
     ft_lambda = sp.lambdify(t, ft, "numpy")
@@ -131,9 +132,9 @@ def lagger(xi, func):
     виде)
      Выходные параметры: список Y"""
 
-    n = 4   # Количество требуемых ненулевых членов
-    ft = 0  # Ряд
-    i = 0   # Итератор
+    n = 4                   # Количество требуемых ненулевых членов
+    ft = 0                  # Ряд
+    i = 0                   # Итератор
     eps = pow(10, -7)
 
     def lagger_f(poly):
@@ -145,7 +146,7 @@ def lagger(xi, func):
 
     while i < n:
         lagger_polynome = lagger_poly(i)    # Нахождение полинома
-        cn = sp.N(sp.integrate(func * lagger_f(lagger_polynome), (t, 0, oo))) # Нахождение Cn
+        cn = sp.N(sp.integrate(func * lagger_f(lagger_polynome), (t, 0, oo)))    # Нахождение Cn
 
         if abs(cn) < eps:
             break
@@ -155,7 +156,7 @@ def lagger(xi, func):
         yp_lambda = sp.lambdify(t, yp, "numpy")
         y = yp_lambda(xi)
 
-        plt.plot(xi, y, label=f"{yp}")  # Добавление элемента на график
+        plt.plot(xi, y, label=f"{yp}")              # Добавление элемента на график
         ft += yp
         i += 1
     ft_lambda = sp.lambdify(t, ft, "numpy")
@@ -169,12 +170,12 @@ chebyshev_func = sp.sympify("3/4*t**3-5/4*t**2+1/4*t")
 lagger_func = sp.sympify(f"-0.3*{e}**(-t)+{e}**(-0.3*t)+0.3*{e}**(-0.3 * t)")
 
 # Списки X
-x = np.linspace(-1, 1, 1000)  # Для Чебышёва и Лежандра
-x2 = np.linspace(0.001, 100, 1000)  # Для Лаггера
+x = np.linspace(-1, 1, 1000)                     # Для Чебышёва и Лежандра
+x2 = np.linspace(0.001, 100, 1000)          # Для Лаггера
 
 # Разложение в ряд Фурье (Лежандр)
-series_l = legandr(x, legandr_func)  # Находим список Y для разложения
-legandr_func_y = [legandr_func.subs(t, i) for i in x]  # Найдем список Y для оригинальной функции
+series_l = legandr(x, legandr_func)                         # Находим список Y для разложения
+legandr_func_y = [legandr_func.subs(t, i) for i in x]       # Найдем список Y для оригинальной функции
 
 # Вывод функции, разложения, разложения суммарно.
 print("Ряд Фурье(Лежандр, отдельные члены разложения)")
